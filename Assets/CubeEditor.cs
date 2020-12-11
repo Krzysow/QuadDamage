890356d12fd764872b5e0c2233e25b60509a489b
Krzysztof Nowacki <krzysow@gmail.com>
2020-12-11 19:30:41 +0100

    Breadth First Search Pathfinding

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour
 {
-    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    Waypoint waypoint;
 
-    TextMesh textMesh;
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
 
     void Update()
     {
-        Vector3 snapPos;
+        SnapToGrid();
+        UpdateLable();
+    }
 
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x * gridSize,
+            0f,
+            waypoint.GetGridPos().y * gridSize);
+    }
 
-        textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    private void UpdateLable()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText =
+            waypoint.GetGridPos().x +
+            "," +
+            waypoint.GetGridPos().y;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
     }
 }
